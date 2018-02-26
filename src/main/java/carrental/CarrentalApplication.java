f37da0fbf2c6d3a607590120665d91f037215497
KRISZ-PC <realantrox@gmail.com>
2018-02-26 19:39:09 +0100

    Init commit

---
+++ src/main/java/carrental/CarrentalApplication.java
@@ -0,0 +1,18 @@
+package carrental;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrentalApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrentalApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		System.out.println("Hello world!");
+	}
+}
